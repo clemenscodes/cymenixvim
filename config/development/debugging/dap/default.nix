3952985ad0b742c8a287201af6752457833faab6
Clemens Horn <clemens.horn@mni.thm.de>
2025-04-06 18:38:24 +0200

    revert whatever the fuck is broken again, these breaking changes are so fking annoying

--- config/development/debugging/dap/default.nix
+++ config/development/debugging/dap/default.nix
@@ -1,41 +1,6 @@
 {pkgs, ...}: {
   extraPlugins = [pkgs.vimPlugins.nvim-nio];
   plugins = {
-    dap-ui = {
-      enable = true;
-      controls = {
-        element = "console";
-      };
-      layouts = [
-        {
-          elements = [
-            {
-              id = "scopes";
-              size = 1;
-            }
-          ];
-          position = "right";
-          size = 100;
-        }
-        {
-          elements = [
-            {
-              id = "repl";
-              size = 0.50;
-            }
-            {
-              id = "console";
-              size = 0.50;
-            }
-          ];
-          position = "bottom";
-          size = 15;
-        }
-      ];
-    };
-    dap-virtual-text = {
-      enable = true;
-    };
     dap = {
       enable = true;
       adapters = {
@@ -54,6 +19,43 @@
       };
       configurations = {
         rust = [];
+      };
+      extensions = {
+        dap-ui = {
+          enable = true;
+          controls = {
+            element = "console";
+          };
+          layouts = [
+            {
+              elements = [
+                {
+                  id = "scopes";
+                  size = 1;
+                }
+              ];
+              position = "right";
+              size = 100;
+            }
+            {
+              elements = [
+                {
+                  id = "repl";
+                  size = 0.50;
+                }
+                {
+                  id = "console";
+                  size = 0.50;
+                }
+              ];
+              position = "bottom";
+              size = 15;
+            }
+          ];
+        };
+        dap-virtual-text = {
+          enable = true;
+        };
       };
     };
     which-key = {
