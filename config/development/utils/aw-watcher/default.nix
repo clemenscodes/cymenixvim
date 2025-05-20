1709f829847afbdfd4eef09ea54e934d998ddbd3
Clemens Horn <clemens.horn@nexobility.de>
2025-05-20 16:21:49 +0200

    feat(aw-watcher): use packaged plugin

--- config/development/utils/aw-watcher/default.nix
+++ config/development/utils/aw-watcher/default.nix
@@ -1,16 +1,12 @@
 {pkgs, ...}: {
-  extraPlugins = [
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "aw-watcher";
-      src = pkgs.fetchFromGitHub {
-        owner = "lowitea";
-        repo = "aw-watcher.nvim";
-        rev = "be7b03748f59b6602502baf08e7f7736cc7279a5";
-        hash = "sha256-YGxlOzigqbpa3pwNDaBJYb7f1NDT8t0O4oB8xgbj+4E=";
+  plugins = {
+    aw-watcher = {
+      enable = true;
+      settings = {
+        host = "127.0.0.1";
+        port = 5600;
+        pulsetime = 1;
       };
-    })
-  ];
-  extraConfigLuaPre = ''
-    require("aw_watcher").setup({})
-  '';
+    };
+  };
 }
