2d5b648f084f235edfd907f487cca8482c60855c
Clemens Horn <clemens.horn@nexobility.de>
2025-05-20 20:03:28 +0200

    tgest

--- config/development/completion/avante/default.nix
+++ config/development/completion/avante/default.nix
@@ -5,6 +5,13 @@
       settings = {
         provider = "openai";
         auto_suggestions_provider = "openai";
+        openai = {
+          endpoint = "https://api.openai.com/v1";
+          model = "gpt-4o";
+          timeout = 30000;
+          temperature = 0;
+          max_completion_tokens = 8192;
+        };
         windows = {
           sidebar_header = {
             align = "center";
