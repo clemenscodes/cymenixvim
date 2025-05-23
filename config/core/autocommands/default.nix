30619660723674b76ecc590cdd7493460ddc54a7
Clemens Horn <clemens.horn@nexobility.de>
2025-05-23 10:46:36 +0200

    fix syntax

--- config/core/autocommands/default.nix
+++ config/core/autocommands/default.nix
@@ -32,54 +32,27 @@
         '';
       };
     }
-
     # Handle performance on large files by https://github.com/khaneliman/khanelivim/blob/main/modules/nixvim/autocommands.nix
-    (
-      lib.mkIf
-      (
-        (!config.plugins.snacks.enable)
-        || (config.plugins.snacks.enable && (!config.plugins.snacks.settings.bigfile.enabled))
-      )
-      {
-        event = "BufEnter";
-        pattern = ["*"];
-        callback = {
-          __raw = ''
-            function()
-              local buf_size_limit = 1024 * 1024 -- 1MB size limit
-              if vim.api.nvim_buf_get_offset(0, vim.api.nvim_buf_line_count(0)) > buf_size_limit then
-                ${lib.optionalString config.plugins.indent-blankline.enable ''require("ibl").setup_buffer(0, { enabled = false })''}
-                ${lib.optionalString (lib.hasAttr "indentscope" config.plugins.mini.modules) ''vim.b.miniindentscope_disable = true''}
-                ${lib.optionalString config.plugins.illuminate.enable ''require("illuminate").pause_buf()''}
-
-                -- Disable line numbers and relative line numbers
-                vim.cmd("setlocal nonumber norelativenumber")
-
-                -- Disable syntax highlighting
-                -- vim.cmd("syntax off")
-
-                -- Disable matchparen
-                vim.cmd("let g:loaded_matchparen = 1")
-
-                -- Disable cursor line and column
-                vim.cmd("setlocal nocursorline nocursorcolumn")
-
-                -- Disable folding
-                vim.cmd("setlocal nofoldenable")
-
-                -- Disable sign column
-                vim.cmd("setlocal signcolumn=no")
-
-                -- Disable swap file and undo file
-                vim.cmd("setlocal noswapfile noundofile")
-
-                -- Disable mini animate
-                vim.b.minianimate_disable = true
-              end
+    {
+      event = "BufEnter";
+      pattern = ["*"];
+      callback = {
+        __raw = ''
+          function()
+            local buf_size_limit = 1024 * 1024 -- 1MB size limit
+            if vim.api.nvim_buf_get_offset(0, vim.api.nvim_buf_line_count(0)) > buf_size_limit then
+              require("ibl").setup_buffer(0, { enabled = false })
+              vim.cmd("setlocal nonumber norelativenumber")
+              vim.cmd("syntax off")
+              vim.cmd("let g:loaded_matchparen = 1")
+              vim.cmd("setlocal nocursorline nocursorcolumn")
+              vim.cmd("setlocal nofoldenable")
+              vim.cmd("setlocal signcolumn=no")
+              vim.cmd("setlocal noswapfile noundofile")
             end
-          '';
-        };
-      }
-    )
+          end
+        '';
+      };
+    }
   ];
 }
