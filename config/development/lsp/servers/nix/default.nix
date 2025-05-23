5ae36b8d66dfc619e547953a8abbad4952c6cd51
Clemens Horn <clemens.horn@nexobility.de>
2025-05-23 14:11:25 +0200

    feat(lsp): use nil again

--- config/development/lsp/servers/nix/default.nix
+++ config/development/lsp/servers/nix/default.nix
@@ -1,10 +1,19 @@
 {...}: {
-  plugins = {
-    lsp = {
-      servers = {
-        nixd = {
-          enable = true;
-          extraOptions = {};
+  lsp = {
+    servers = {
+      nil_ls = {
+        enable = true;
+        settings = {
+          settings = {
+            formatting = {
+              command = ["${lib.getExe pkgs.nixfmt-rfc-style}"];
+            };
+            nix = {
+              flake = {
+                autoArchive = true;
+              };
+            };
+          };
         };
       };
     };
