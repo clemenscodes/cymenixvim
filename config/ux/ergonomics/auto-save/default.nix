b43c0486d47eedc733e3b751ca3c9ebfe5e1eddc
Clemens Horn <clemens.horn@nexobility.de>
2025-05-23 12:22:09 +0200

    feat(auto-save): more defensive

--- config/ux/ergonomics/auto-save/default.nix
+++ config/ux/ergonomics/auto-save/default.nix
@@ -4,14 +4,14 @@
       enable = true;
       settings = {
         enabled = true;
-        debounce_delay = 2000;
+        debounce_delay = 3000;
         lockmarks = true;
         noautocmd = true;
         writeAllBuffers = false;
         trigger_events = {
           cancel_deferred_save = ["InsertEnter"];
-          defer_save = ["InsertLeave" "TextChanged"];
-          immediate_save = ["BufLeave" "FocusLost" "QuitPre" "VimSuspend"];
+          defer_save = ["BufLeave" "FocusLost"];
+          immediate_save = ["QuitPre" "VimSuspend"];
         };
       };
     };
