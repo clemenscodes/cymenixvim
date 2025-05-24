29f1b60f7c8b288ac0ad077926b1c5e7b9df4691
Clemens Horn <clemens.horn@nexobility.de>
2025-05-24 12:13:06 +0200

    fix(symbols): remove dupl import

--- config/development/lsp/symbols/symbol-usage/default.nix
+++ config/development/lsp/symbols/symbol-usage/default.nix
@@ -1,12 +1,4 @@
-{
-  inputs,
-  pkgs,
-  lib,
-  ...
-}: {
-  imports = [
-    (import ./symbol-usage {inherit inputs pkgs lib;})
-  ];
+{pkgs, ...}: {
   extraPlugins = [
     (pkgs.vimUtils.buildVimPlugin {
       name = "symbol-usage";
