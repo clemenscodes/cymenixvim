00d1e6fc7e2aa200da5410c20d6af20eb7ebac54
Clemens Horn <clemens.horn@mni.thm.de>
2025-05-25 11:11:21 +0200

    feat(hardtime): allow hjkl navigation

--- config/ux/navigation/hardtime/default.nix
+++ config/ux/navigation/hardtime/default.nix
@@ -2,6 +2,19 @@
   plugins = {
     hardtime = {
       enable = true;
+      settings = {
+        enabled = true;
+        restricted_keys = {
+          "+" = ["n" "x"];
+          "-" = ["n" "x"];
+          "<C-M>" = ["n" "x"];
+          "<C-N>" = ["n" "x"];
+          "<C-P>" = ["n" "x"];
+          "<CR>" = ["n" "x"];
+          "gj" = ["n" "x"];
+          "gk" = ["n" "x"];
+        };
+      };
     };
     which-key = {
       settings = {
@@ -18,7 +31,7 @@
     {
       mode = "n";
       key = "<leader>th";
-      action = ":Hardtime toggle";
+      action = ":Hardtime toggle<CR>";
       options = {
         silent = true;
         desc = "Toggle Hardtime";
