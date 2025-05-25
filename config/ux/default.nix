e4bd1870c7d28e0f838c88c35e14c261f269aafe
Clemens Horn <clemens.horn@mni.thm.de>
2025-05-25 13:23:54 +0200

    test

--- config/ux/default.nix
+++ config/ux/default.nix
@@ -8,7 +8,6 @@
     (import ./diagnostics {inherit inputs pkgs lib;})
     (import ./ergonomics {inherit inputs pkgs lib;})
     (import ./navigation {inherit inputs pkgs lib;})
-    (import ./organization {inherit inputs pkgs lib;})
     (import ./search {inherit inputs pkgs lib;})
     (import ./sessions {inherit inputs pkgs lib;})
     (import ./shortcuts {inherit inputs pkgs lib;})
