387d42f646c6442abc74c7f99c4d4d0a968f3218
Clemens Horn <clemens.horn@mni.thm.de>
2025-05-27 19:44:33 +0200

    feat(tailwind): use tailwind-tools

--- config/development/lsp/servers/tailwind/default.nix
+++ config/development/lsp/servers/tailwind/default.nix
@@ -1,26 +1,7 @@
 {...}: {
   plugins = {
-    lsp = {
-      servers = {
-        tailwindcss = {
-          enable = true;
-          filetypes = [
-            "typescript"
-            "html"
-            "typescriptreact"
-            "typescript.tsx"
-          ];
-          extraOptions = {
-            rootDir =
-              /*
-              lua
-              */
-              ''
-                require('lspconfig').util.root_pattern(".git")
-              '';
-          };
-        };
-      };
+    tailwind-tools = {
+      enable = true;
     };
   };
 }
