46e5fbf89e24c7816d671b84bc8098043a5d145c
Clemens Horn <clemens.horn@nexobility.de>
2025-05-27 08:18:42 +0200

    fix(lua): remove neodev

--- config/development/utils/default.nix
+++ config/development/utils/default.nix
@@ -6,7 +6,6 @@
 }: {
   imports = [
     (import ./aw-watcher {inherit inputs pkgs lib;})
-    (import ./neodev {inherit inputs pkgs lib;})
     (import ./treesitter {inherit inputs pkgs lib;})
     (import ./zen-mode {inherit inputs pkgs lib;})
   ];
