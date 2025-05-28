d9aab404fc2160be1a2faa276ea14235ec14e939
Clemens Horn <clemens.horn@nexobility.de>
2025-05-28 10:27:22 +0200

    feat(lspkind): more cmp stuff

--- config/development/lsp/default.nix
+++ config/development/lsp/default.nix
@@ -15,6 +15,26 @@
     };
     lspkind = {
       enable = true;
+      cmp = {
+        enable = true;
+        menu = {
+          buffer = "";
+          calc = "";
+          cmdline = "";
+          codeium = "󱜙";
+          copilot = "";
+          emoji = "󰞅";
+          git = "";
+          luasnip = "󰩫";
+          neorg = "";
+          nvim_lsp = "";
+          nvim_lua = "";
+          path = "";
+          spell = "";
+          treesitter = "󰔱";
+          nixpkgs_maintainers = "";
+        };
+      };
     };
     which-key = {
       settings = {
