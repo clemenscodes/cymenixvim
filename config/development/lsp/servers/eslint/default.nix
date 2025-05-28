ac2d60e45b6b26cfe4ff88e9616b9a7ca084648c
Clemens Horn <clemens.horn@nexobility.de>
2025-05-28 12:16:10 +0200

    test

--- config/development/lsp/servers/eslint/default.nix
+++ config/development/lsp/servers/eslint/default.nix
@@ -3,7 +3,7 @@
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     root_dir = vim.fn.getcwd(),
-    require'lspconfig'.eslint.setup {
+    require('lspconfig').eslint.setup {
       capabilities = capabilites,
       root_dir = root_dir,
     }
