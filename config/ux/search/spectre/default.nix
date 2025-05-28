3ecf78a9ef2a65af3063103b9ed7e5ee393dbe5d
Clemens Horn <clemens.horn@nexobility.de>
2025-05-28 19:50:07 +0200

    feat(spectre): test config

--- config/ux/search/spectre/default.nix
+++ config/ux/search/spectre/default.nix
@@ -1,8 +1,61 @@
 {pkgs, ...}: {
-  extraPackages = [(pkgs.writeShellScriptBin "gsed" "exec ${pkgs.gnused}/bin/sed \"$@\"")];
+  extraPackages = [
+    pkgs.gnused
+    (pkgs.writeShellScriptBin "gsed" "exec ${pkgs.gnused}/bin/sed \"$@\"")
+  ];
   plugins = {
     spectre = {
       enable = true;
+      settings = {
+        default = {
+          find = {
+            cmd = "rg";
+            options = [
+              "word"
+              "hidden"
+            ];
+          };
+          replace = {
+            cmd = "sed";
+          };
+        };
+        find_engine = {
+          rg = {
+            args = [
+              "--color=never"
+              "--no-heading"
+              "--with-filename"
+              "--line-number"
+              "--column"
+            ];
+            cmd = "rg";
+            options = {
+              hidden = {
+                desc = "hidden file";
+                icon = "[H]";
+                value = "--hidden";
+              };
+              ignore-case = {
+                desc = "ignore case";
+                icon = "[I]";
+                value = "--ignore-case";
+              };
+              line = {
+                desc = "match in line";
+                icon = "[L]";
+                value = "-x";
+              };
+              word = {
+                desc = "match in word";
+                icon = "[W]";
+                value = "-w";
+              };
+            };
+          };
+        };
+        is_insert_mode = false;
+        live_update = true;
+      };
     };
     which-key = {
       settings = {
