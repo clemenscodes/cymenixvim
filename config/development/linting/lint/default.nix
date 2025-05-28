0d4a555b900b30c035d11d1c584d50d7c55066d8
Clemens Horn <clemens.horn@nexobility.de>
2025-05-28 10:34:35 +0200

    fix(lint): proper invoke

--- config/development/linting/lint/default.nix
+++ config/development/linting/lint/default.nix
@@ -9,7 +9,7 @@
         # Lua
         ''
           function()
-            lint.try_lint()
+            require('lint').try_lint()
           end
         '';
       key = "<leader>l";
@@ -44,6 +44,8 @@
         htmlhint.cmd = lib.getExe pkgs.htmlhint;
         jsonlint.cmd = lib.getExe pkgs.nodePackages.jsonlint;
         luacheck.cmd = lib.getExe pkgs.luaPackages.luacheck;
+        eslint_d.cmd = lib.getExe pkgs.eslint_d;
+        eslint.cmd = lib.getExe pkgs.eslint;
         markdownlint.cmd = lib.getExe pkgs.markdownlint-cli;
         nix.cmd = lib.getExe' pkgs.nix "nix-instantiate";
         shellcheck.cmd = lib.getExe pkgs.shellcheck;
