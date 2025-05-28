179c49d52743c2c0b0683c1b10fce0c3f90c7ca1
Clemens Horn <clemens.horn@nexobility.de>
2025-05-28 13:27:19 +0200

    test

--- config/development/lsp/servers/eslint/default.nix
+++ config/development/lsp/servers/eslint/default.nix
@@ -6,6 +6,18 @@
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     lsp.eslint.setup {
       capabilities = capabilites,
+      root_dir = function(fname)
+        if fname:match("/node_modules/") then
+          return nil
+        end
+        return require('lspconfig.util').root_pattern(".git", "nx.json")(fname)
+      end,
+      on_attach = function(client, bufnr)
+        local filepath = vim.api.nvim_buf_get_name(bufnr)
+        if filepath:match("/node_modules/") then
+          client.stop()
+        end
+      end,
     }
   '';
 }
