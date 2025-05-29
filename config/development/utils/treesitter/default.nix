bdcf3bee4006f7d955fe22ef20633bbcb88119c6
Clemens Horn <clemens.horn@nexobility.de>
2025-05-29 13:24:35 +0200

    feat(treesitter): add binds for textobjects

--- config/development/utils/treesitter/default.nix
+++ config/development/utils/treesitter/default.nix
@@ -76,6 +76,183 @@ in {
       };
       move = {
         enable = true;
+        setJumps = true;
+        gotoNextStart = {
+          "nfo" = {
+            query = "@function.outer";
+            desc = "Next function (outer)";
+          };
+          "nfi" = {
+            query = "@function.inner";
+            desc = "Next function (inner)";
+          };
+          "nco" = {
+            query = "@class.outer";
+            desc = "Next class (outer)";
+          };
+          "nci" = {
+            query = "@class.inner";
+            desc = "Next class (inner)";
+          };
+          "nlo" = {
+            query = "@loop.outer";
+            desc = "Next loop (outer)";
+          };
+          "nli" = {
+            query = "@loop.inner";
+            desc = "Next loop (inner)";
+          };
+          "nso" = {
+            query = "@local.scope";
+            queryGroup = "locals";
+            desc = "Next local scope";
+          };
+          "nzo" = {
+            query = "@fold";
+            queryGroup = "folds";
+            desc = "Next fold";
+          };
+          "nio" = {
+            query = "@conditional.outer";
+            desc = "Next conditional (outer)";
+          };
+          "nii" = {
+            query = "@conditional.inner";
+            desc = "Next conditional (inner)";
+          };
+        };
+        gotoNextEnd = {
+          "nFo" = {
+            query = "@function.outer";
+            desc = "End of next function (outer)";
+          };
+          "nFi" = {
+            query = "@function.inner";
+            desc = "End of next function (inner)";
+          };
+          "nCo" = {
+            query = "@class.outer";
+            desc = "End of next class (outer)";
+          };
+          "nCi" = {
+            query = "@class.inner";
+            desc = "End of next class (inner)";
+          };
+          "nLo" = {
+            query = "@loop.outer";
+            desc = "End of next loop (outer)";
+          };
+          "nLi" = {
+            query = "@loop.inner";
+            desc = "End of next loop (inner)";
+          };
+          "nSo" = {
+            query = "@local.scope";
+            queryGroup = "locals";
+            desc = "End of next local scope";
+          };
+          "nUo" = {
+            query = "@fold";
+            queryGroup = "folds";
+            desc = "End of next fold";
+          };
+          "nIo" = {
+            query = "@conditional.outer";
+            desc = "End of next conditional (outer)";
+          };
+          "nIi" = {
+            query = "@conditional.inner";
+            desc = "End of next conditional (inner)";
+          };
+        };
+        gotoPreviousStart = {
+          "mfo" = {
+            query = "@function.outer";
+            desc = "Previous function (outer)";
+          };
+          "mfi" = {
+            query = "@function.inner";
+            desc = "Previous function (inner)";
+          };
+          "mco" = {
+            query = "@class.outer";
+            desc = "Previous class (outer)";
+          };
+          "mci" = {
+            query = "@class.inner";
+            desc = "Previous class (inner)";
+          };
+          "mlo" = {
+            query = "@loop.outer";
+            desc = "Previous loop (outer)";
+          };
+          "mli" = {
+            query = "@loop.inner";
+            desc = "Previous loop (inner)";
+          };
+          "mso" = {
+            query = "@local.scope";
+            queryGroup = "locals";
+            desc = "Previous local scope";
+          };
+          "mzo" = {
+            query = "@fold";
+            queryGroup = "folds";
+            desc = "Previous fold";
+          };
+          "mio" = {
+            query = "@conditional.outer";
+            desc = "Previous conditional (outer)";
+          };
+          "mii" = {
+            query = "@conditional.inner";
+            desc = "Previous conditional (inner)";
+          };
+        };
+        gotoPreviousEnd = {
+          "mFo" = {
+            query = "@function.outer";
+            desc = "End of previous function (outer)";
+          };
+          "mFi" = {
+            query = "@function.inner";
+            desc = "End of previous function (inner)";
+          };
+          "mCo" = {
+            query = "@class.outer";
+            desc = "End of previous class (outer)";
+          };
+          "mCi" = {
+            query = "@class.inner";
+            desc = "End of previous class (inner)";
+          };
+          "mLo" = {
+            query = "@loop.outer";
+            desc = "End of previous loop (outer)";
+          };
+          "mLi" = {
+            query = "@loop.inner";
+            desc = "End of previous loop (inner)";
+          };
+          "mSo" = {
+            query = "@local.scope";
+            queryGroup = "locals";
+            desc = "End of previous local scope";
+          };
+          "mUo" = {
+            query = "@fold";
+            queryGroup = "folds";
+            desc = "End of previous fold";
+          };
+          "mIo" = {
+            query = "@conditional.outer";
+            desc = "End of previous conditional (outer)";
+          };
+          "mIi" = {
+            query = "@conditional.inner";
+            desc = "End of previous conditional (inner)";
+          };
+        };
       };
       select = {
         enable = true;
