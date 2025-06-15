4ad67e04511a1a97b561f05d0f025166180f5f36
Clemens Horn <clemens.horn@mni.thm.de>
2025-06-15 14:48:18 +0200

    update python lsp configs

--- config/development/lsp/servers/python/default.nix
+++ config/development/lsp/servers/python/default.nix
@@ -1,11 +1,52 @@
-{...}: {config, ...}: {
-  plugins = {
-    lsp = {
-      servers = {
+{pkgs, ...}: {config, ...}: {
+  extraConfigLuaPost = ''
+    require("conform").setup({
+        formatters_by_ft = {
+            python = {
+              -- To fix auto-fixable lint errors.
+              "ruff_fix",
+              -- To run the Ruff formatter.
+              "ruff_format",
+              -- To organize the imports.
+              "ruff_organize_imports",
+            },
+        },
+    })
+    require("lint").linters_by_ft = {
+      python = { "ruff" },
+    }
+    local lsp = require('lspconfig')
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+      properties = {
+        "documentation",
+        "detail",
+        "additionalTextEdits",
+      },
+    }
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+    lsp.pyright.setup {
+      capabilities = capabilites,
+      root_dir = vim.fn.getcwd(),
+      settings = {
         pyright = {
-          enable = true;
-        };
-      };
-    };
-  };
+          disableOrganizeImports = true, -- Using Ruff
+          disableTaggedHints = true,
+        },
+        python = {
+          analysis = {
+           diagnosticSeverityOverrides = {
+              -- https://github.com/microsoft/pyright/blob/main/docs/configuration.md#type-check-diagnostics-settings
+              reportUndefinedVariable = "none",
+            },
+          },
+        },
+      },
+    }
+  '';
 }
