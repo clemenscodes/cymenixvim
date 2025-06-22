b08a591d96ffa290018dd09d3fafcfabcc016bc3
Clemens Horn <clemens.horn@mni.thm.de>
2025-06-22 20:04:09 +0200

    disable rustowl by default

--- config/development/languages/rust/default.nix
+++ config/development/languages/rust/default.nix
@@ -7,6 +7,5 @@
   imports = [
     (import ./crates-nvim {inherit inputs pkgs lib;})
     (import ./rustaceanvim {inherit inputs pkgs lib;})
-    (import ./rustowl {inherit inputs pkgs lib;})
   ];
 }
