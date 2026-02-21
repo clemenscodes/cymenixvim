abf1a4b1f75b62eecf0db3ff0b696626a007e948
Clemens Horn <me@clemenshorn.com>
2026-02-21 11:22:24 +0100

    remove broken mesonlsp setup

--- config/development/lsp/servers/c/default.nix
+++ config/development/lsp/servers/c/default.nix
@@ -1,16 +1,4 @@
-{pkgs, ...}: {
-  extraPackages = [pkgs.mesonlsp];
-  extraConfigLuaPost = ''
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
-    root_dir = vim.fn.getcwd(),
-    vim.lsp.config.mesonlsp.setup {
-      capabilities = capabilites,
-      root_dir = root_dir,
-      filetypes = { 'meson' },
-      cmd = {'mesonlsp', '--lsp' }
-    }
-  '';
+{...}: {
   lsp.servers.clangd = {
     enable = true;
     settings = {
